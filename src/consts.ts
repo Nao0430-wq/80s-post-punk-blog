49c69b9a4248d1d1aea7134276f91aea2386cab0
犬伏 直生 <noelandliam821@Mac-mini.local>
2026-02-23 15:58:29 +0900

    Initial commit of 80s Post-Punk Blog

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = '80s Post-Punk Archives';
+export const SITE_DESCRIPTION = 'The dark aesthetics and hidden gems of 80s Post-Punk and beyond.';
